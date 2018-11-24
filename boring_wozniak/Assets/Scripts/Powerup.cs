3ea0260343665d82c6ef7de3ec16a3f2bf5a9dbd
Azuthedar <Johann.Buys97@gmail.com>
2018-11-24 15:44:02 +0200

    Updated prefab

--- boring_wozniak/Assets/Scripts/Powerup.cs
+++ boring_wozniak/Assets/Scripts/Powerup.cs
@@ -6,16 +6,12 @@ public class Powerup : MonoBehaviour {
 	// Use this for initialization
 	[SerializeField]
 	public float duration = 30f;
-	private GameObject trump;
 
 	void Start () {
-		trump = GameObject.Find("Trump");
-		this.transform.rotation = Random.rotation;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		this.transform.RotateAround(this.transform.position, new Vector3(0,0,0), Time.deltaTime * 15.5f);
 	}
 
 
