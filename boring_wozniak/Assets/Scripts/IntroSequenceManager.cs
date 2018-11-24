247fdb56202e993a02d2cdcc184f1743077b1beb
TianeErwee <tiane.erwee@standardbank.co.za>
2018-11-24 10:44:34 +0200

    Intro sequence animation and scen
    Intro sequence animation

---
+++ boring_wozniak/Assets/Scripts/IntroSequenceManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroSequenceManager : MonoBehaviour {
+
+	public static IntroSequenceManager instance;
+
+	public GameObject[] introSequencePanels;
+
+	private int currentPos = 0;
+
+	// Use this for initialization
+	void Start () {
+		instance = this;
+
+        Animator panelAnimator = (Animator)introSequencePanels[currentPos].GetComponent("Animator");
+        panelAnimator.SetBool("slide_in", true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			updateIntroSequence();
+		}
+	}
+
+	void updateIntroSequence () {
+		if (currentPos < 3) {
+			Animator panelAnimator = (Animator)introSequencePanels[currentPos].GetComponent("Animator");
+            panelAnimator.SetBool("slide_out", true);
+			currentPos++;
+			panelAnimator = (Animator)introSequencePanels[currentPos].GetComponent("Animator");
+			panelAnimator.SetBool("slide_in", true);
+		}
+	}
+}
